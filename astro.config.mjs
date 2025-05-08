3f86e1fafc149aeccfaff3a5c094c48053db269f
Francisco Paz <fjpazf@unah.hn>
2025-05-08 17:13:07 -0400

    despliegue inicial

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+     site: 'https://orocuina.com',
+  // site: 'https://Franciscopaz199.github.io', 
+  // base: 'personallandingpage',
   vite: {
     plugins: [tailwindcss()]
   }
